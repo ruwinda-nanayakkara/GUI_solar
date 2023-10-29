c6f4603f4ca53dfa9f3705c6fc15fe60cd43db1e
Pallege Sasith Ruwinda Nanayakkara <62781016+ruwinda-nanayakkara@users.noreply.github.com>
2023-10-29 18:58:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def measure():
     "Measure" button calls this function
     '''
     for var in vars:
-        var.set('A')
+        var[0].set('A')
+        var[1].set('B')
 
 # window
 window = tk.Tk()
@@ -28,9 +29,12 @@ for i in range(10):
     cell_frame.grid(row=i//2+1, column=i%2, padx=10, pady=10)
 
     # textvariable
-    var = tk.StringVar()
-    var.set('0')
-    vars.append(var)
+    var_v = tk.StringVar()
+    var_v.set('0')
+
+    var_i = tk.StringVar()
+    var_i.set('0.0')
+    vars.append([var_v, var_i])
 
     v_label = tk.Label(master=cell_frame, text='OCV(V) ',relief='groove', justify='left',padx=5)
     i_label = tk.Label(master=cell_frame, text='SCC(mA)',relief='groove', justify='left',padx=5)
@@ -38,8 +42,8 @@ for i in range(10):
     v_label.grid(row=1,column=1, padx=10, pady=5, sticky='W')
     i_label.grid(row=2,column=1, padx=10, pady=5)
 
-    v_data_lbl = tk.Label(master=cell_frame, textvariable=vars[i], relief='sunken', width=4)
-    i_data_lbl = tk.Label(master=cell_frame, textvariable=vars[i], relief='sunken', width=4)
+    v_data_lbl = tk.Label(master=cell_frame, textvariable=vars[i][0], relief='sunken', width=4)
+    i_data_lbl = tk.Label(master=cell_frame, textvariable=vars[i][1], relief='sunken', width=4)
 
     v_data_lbl.grid(row=1, column=2, padx=10, pady=5)
     i_data_lbl.grid(row=2, column=2, padx=10, pady=5)
