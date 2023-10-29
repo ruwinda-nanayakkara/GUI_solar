f3ba3e96ff357ca6cff8f0e95b3ac84ef31ad90e
Pallege Sasith Ruwinda Nanayakkara <62781016+ruwinda-nanayakkara@users.noreply.github.com>
2023-10-29 18:48:06 +0530

    tkinter base

---
+++ main.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import ttk
+
+vars = [] # tk.StringVar() array
+
+def measure():
+    '''
+    "Measure" button calls this function
+    '''
+    for var in vars:
+        var.set('A')
+
+# window
+window = tk.Tk()
+window.title("SolarCell Tester v0.1")
+# window.geometry("400x200")
+
+# title
+title_label = ttk.Label(master=window, text="SolarCell Tester", font='Calibri 24 bold')
+title_label.grid(row=0,column=0,columnspan=2,)
+
+
+
+# cells
+for i in range(10):
+    #one cell
+    cell_frame = tk.LabelFrame(master=window, text='Cell {cell:d}'.format(cell=i+1))
+    cell_frame.grid(row=i//2+1, column=i%2, padx=10, pady=10)
+
+    # textvariable
+    var = tk.StringVar()
+    var.set('0')
+    vars.append(var)
+
+    v_label = tk.Label(master=cell_frame, text='OCV(V) ',relief='groove', justify='left',padx=5)
+    i_label = tk.Label(master=cell_frame, text='SCC(mA)',relief='groove', justify='left',padx=5)
+
+    v_label.grid(row=1,column=1, padx=10, pady=5, sticky='W')
+    i_label.grid(row=2,column=1, padx=10, pady=5)
+
+    v_data_lbl = tk.Label(master=cell_frame, textvariable=vars[i], relief='sunken', width=4)
+    i_data_lbl = tk.Label(master=cell_frame, textvariable=vars[i], relief='sunken', width=4)
+
+    v_data_lbl.grid(row=1, column=2, padx=10, pady=5)
+    i_data_lbl.grid(row=2, column=2, padx=10, pady=5)
+
+# buttons
+measr_btn = tk.Button(master=window, text='Measure', command=measure)
+measr_btn.grid(row=13,column=1,padx=10,pady=10, sticky='E')
+
+# run 
+window.mainloop()
